e1873d552c8bab02e2fdc91c45c46d793b6ff778
Isaac Martins <isaacmartins431@gmail.com>
2026-04-08 16:44:08 -0300

    fix: corrige caminhos de importacao no app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { todoReducer, TodoEffects } from './exercicios/todo.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore({ todo: todoReducer }),
+    provideEffects([TodoEffects])
   ]
 };
